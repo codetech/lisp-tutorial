0376a0559b8b2577255e5aa2616c7e3340f57acb
Jackson Ray Hamilton <jackson@jacksonrayhamilton.com>
2015-04-08 02:26:57 -0700

    Add some goodies to the init file.

--- init.el
+++ init.el
@@ -1,3 +1,18 @@
 (add-to-list 'load-path "~/.emacs.d/lisp")
+
+;; Use modern keyboard shortcuts.
 (require 'modern)
 (modern-mode)
+
+;; Match parentheses.
+(require 'paren)
+(show-paren-mode)
+(setq show-paren-delay 0)
+
+;; Automatically pair parentheses, quotation marks, etc.
+;; (electric-pair-mode)
+
+;; Quickly switch between open files and navigate through directories.
+;; (setq ido-enable-flex-matching t)
+;; (setq ido-everywhere t)
+;; (ido-mode 1)
